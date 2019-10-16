0cadc4ed5461525e2a09a4acc7b43a6536e025ea
Marcin <mwandalowski@wp.pl>
2019-10-16 23:29:51 +0200

    LoadMore Button JS created

--- js/app.js
+++ js/app.js
@@ -46,3 +46,43 @@ const buttonValidator = () => {
 };
 
 mailButton.addEventListener("click", buttonValidator);
+
+// loading more posts
+
+const loadMoreButton = document.querySelector(
+  ".posts-second__load-more button"
+);
+const loadMoreContainer = document.querySelector(".posts-second__load-more");
+const postsSecondSection = document.querySelector(".posts-second .row");
+
+const loadMore = () => {
+  // new elements to create
+  const newLink = document.createElement("a");
+  const newPostDiv = document.createElement("div");
+  const newImg = document.createElement("img");
+  const newHeadline5 = document.createElement("h5");
+  const newHeadline1 = document.createElement("h1");
+  const newParagraph = document.createElement("p");
+
+  // appending those elements to the website
+
+  postsSecondSection.insertBefore(newLink, loadMoreContainer);
+  newLink.setAttribute("href", "#");
+  newLink.classList.add("col-sm-12", "col-md-6", "col-lg-4");
+  newLink.appendChild(newPostDiv);
+  newPostDiv.classList.add("posts-second__post");
+  newPostDiv.appendChild(newImg);
+  newImg.setAttribute("src", "./images/image8.jpg");
+  newImg.setAttribute("alt", "Dog");
+  newPostDiv.appendChild(newHeadline5);
+  newHeadline5.innerText = "Lifestyle";
+  newPostDiv.appendChild(newHeadline1);
+  newHeadline1.innerText = "Lorem ipsum, lorem ipsum";
+  newPostDiv.appendChild(newParagraph);
+  newParagraph.innerText =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed nibh ac mi accumsan malesuada eu eget mauris. Suspendisse viverra, ligula et euismod hendrerit, massa dolor bibendum leo, ac pulvinar quam diam sit amet mauris.";
+  loadMoreButton.parentElement.removeChild(loadMoreButton);
+  loadMoreContainer.style.padding = "3rem 0 0 0";
+};
+
+loadMoreButton.addEventListener("click", loadMore);
